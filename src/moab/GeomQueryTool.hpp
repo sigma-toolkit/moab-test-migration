a8727969601f4bb7d13ad2989fa4ff454474f4b8
Patrick Shriwise <pshriwise@gmail.com>
2019-03-25 17:04:04 -0500

    Adding find_volume_slow (uses PIV loop). Should help with clarity, in find_volume.

--- src/moab/GeomQueryTool.hpp
+++ src/moab/GeomQueryTool.hpp
@@ -204,6 +204,20 @@ public:
                         EntityHandle& volume,
                         const double *dir = nullptr);
 
+  /**\brief Find volume for a given location using loop. (slow)
+   *
+   * Loops over all volumes in the model, checking for point containment
+   * @param xyz The location to test
+   * @param volume Set to volume handle containing the location if found
+   * @param dir Optional direction to use for underlying ray fire query.  Used to ensure
+   *        consistent results when a ray direction is known.  If NULL or {0,0,0} is
+   *        given, a random direction will be used.
+   */
+  ErrorCode find_volume_slow(const double xyz[3],
+                        EntityHandle& volume,
+                        const double *dir = nullptr);
+
+
   /**\brief Test if a point is inside or outsize a volume's axis-aligned bounding box
    *
    * This is used as a fast way to determine whether a point is inside or outside of a volume
