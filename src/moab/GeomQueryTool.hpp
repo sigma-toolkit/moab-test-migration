9923b064bb29d59e455262015898a2980f4059db
Patrick Shriwise <pshriwise@gmail.com>
2019-03-25 16:16:13 -0500

    Adding initial version of GQT find_volume

--- src/moab/GeomQueryTool.hpp
+++ src/moab/GeomQueryTool.hpp
@@ -190,6 +190,20 @@ public:
    */
   ErrorCode point_in_volume_slow( const EntityHandle volume, const double xyz[3], int& result );
 
+  /**\brief Find volume for a given location.
+   *
+   * Determines which volume contains the point if possible. If no volume is found,
+   * a null EntityHandle is returned along with a MB_ENTITY_NOT_FOUND ErrorCode.
+   * @param xyz The location to test
+   * @param volume Set to volume handle containing the location if found
+   * @param dir Optional direction to use for underlying ray fire query.  Used to ensure
+   *        consistent results when a ray direction is known.  If NULL or {0,0,0} is
+   *        given, a random direction will be used.
+   */
+  ErrorCode find_volume(const double xyz[3],
+                        EntityHandle& volume,
+                        const double *dir = nullptr);
+
   /**\brief Test if a point is inside or outsize a volume's axis-aligned bounding box
    *
    * This is used as a fast way to determine whether a point is inside or outside of a volume
