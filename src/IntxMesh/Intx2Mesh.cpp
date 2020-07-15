c7f99fd75c6a7a1a0489c453aa748536b78745de
Iulian Grindeanu <iulian@mcs.anl.gov>
2020-07-15 00:16:20 -0500

    format properly

--- src/IntxMesh/Intx2Mesh.cpp
+++ src/IntxMesh/Intx2Mesh.cpp
@@ -133,8 +133,7 @@ ErrorCode Intx2Mesh::createTags()
     std::vector< EntityHandle > zeroh( max_edges_2, 0 );
     // if we have a tag with this name, it could be of a different size, so delete it
     rval = mb->tag_get_handle( "__tgtEdgeNeighbors", neighTgtEdgeTag );
-    if (rval == MB_SUCCESS && neighTgtEdgeTag )
-        mb->tag_delete( neighTgtEdgeTag );
+    if( rval == MB_SUCCESS && neighTgtEdgeTag ) mb->tag_delete( neighTgtEdgeTag );
     rval = mb->tag_get_handle( "__tgtEdgeNeighbors", max_edges_2, MB_TYPE_HANDLE, neighTgtEdgeTag,
                                MB_TAG_DENSE | MB_TAG_CREAT, &zeroh[0] );MB_CHK_SET_ERR( rval, "can't create tgt edge neighbors tag" );
     for( Range::iterator rit = rs2.begin(); rit != rs2.end(); rit++ )
