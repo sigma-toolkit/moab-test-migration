ef2f19d767e197dd6eba18eb8996f27adbfed659
Paul P.H. Wilson <paul.wilson@wisc.edu>
2020-08-15 21:31:08 -0500

    Update to support python3 in ubuntu20.04
    There is no python executable, only python2 and
    python3.  This fix uses python if found (older
    python2 system) otherwise looks for python3.
    This fix comes from @kkiesling in #123

--- pymoab/install.sh.cmake
+++ pymoab/install.sh.cmake
@@ -8,4 +8,11 @@ if [[ ! -d $PYMOAB_INSTALL_PREFIX ]]; then
   mkdir -p @PYMOAB_INSTALL_PREFIX@
 fi
 
-python setup.py install --prefix=@CMAKE_INSTALL_PREFIX@ --record @PYMOAB_INSTALL_PREFIX@/install_files.txt
+
+if [ -x "$(command -v python)" ]; then
+  python_exe=python
+elif [ -x "$(command -v python3)" ]; then
+  python_exe=python3
+fi
+
+${python_exe} setup.py install --prefix=@CMAKE_INSTALL_PREFIX@ --record @PYMOAB_INSTALL_PREFIX@/install_files.txt
