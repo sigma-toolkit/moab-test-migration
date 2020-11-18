82fc8de7612e7d1a4247e7419fba9a39aff91dde
Iulian Grindeanu <iulian@mcs.anl.gov>
2020-11-18 16:17:32 -0600

    do a test of some value
    more checks to see if gnomonic projection worked

--- test/gnom_project_test.cpp
+++ test/gnom_project_test.cpp
@@ -42,6 +42,15 @@ int main( int  argc, char*  argv[] )
     rval = mb->create_meshset( MESHSET_SET, outSet );MB_CHK_ERR( rval );
 
     rval = IntxUtils::global_gnomonic_projection( mb, sf,  R, false, outSet );MB_CHK_ERR( rval );
+    // check first cell position
+    Range cells;
+    rval = mb->get_entities_by_dimension(outSet, 2, cells);MB_CHK_ERR( rval );
+    EntityHandle firstCell = cells[0];
+    double coords[3];
+    rval  = mb->get_coords(&firstCell, 1, coords); MB_CHK_ERR( rval );
+    // check position
+    double values[3]={-0.78867513420303437, 0.78867513420303437, 0};
+    CHECK_ARRAYS_EQUAL(coords, 3, values, 3);
 
     rval = mb->write_file(fileout.c_str(), 0, 0, &outSet, 1); ;MB_CHK_ERR( rval );
 
