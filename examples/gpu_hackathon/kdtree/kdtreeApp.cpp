56e1396ef23ab0091857a81c29a915178bef0e76
Vijay Mahadevan <vijay.m@gmail.com>
2021-04-19 13:17:52 -0400

    Some comments for Kdtree for GPU

--- examples/gpu_hackathon/kdtree/kdtreeApp.cpp
+++ examples/gpu_hackathon/kdtree/kdtreeApp.cpp
@@ -56,13 +56,13 @@ std::map< std::string, std::chrono::nanoseconds > timeLog;
 int main( int argc, char** argv )
 {
     std::string test_file_name  = string( MESH_DIR ) + string( "/64bricks_512hex_256part.h5m" );
-    int num_queries             = 10000;
+    int num_queries             = 1000000;
     int dimension               = 3;
     int uniformRefinementLevels = 0;
 
     ProgOptions opts;
     // Need option handling here for input filename
-    opts.addOpt< int >( "dim,d", "Dimension of the problem and mesh (default=1)", &dimension );
+    opts.addOpt< int >( "dim,d", "Dimension of the problem and mesh (default=3)", &dimension );
     opts.addOpt< int >( "queries,n", "Number of queries to perform on the mesh (default=1E4)", &num_queries );
     opts.addOpt< int >( "refine,r", "Number of levels of uniform refinements to perform on the mesh (default=0)",
                         &uniformRefinementLevels );
@@ -103,6 +103,8 @@ int main( int argc, char** argv )
 
     PUSH_TIMER()
     // Create a tree to use for the location service
+    // Can we acclerate this setup phase on a GPU as well ??
+    // Or may be use Kokkos/OpenMP for CPU executor ?
     AdaptiveKDTree tree(&mb, elems, &fileset);
 
     // Build the SpatialLocator
@@ -115,16 +117,21 @@ int main( int argc, char** argv )
     BoundBox box = sl.local_box();
     box_extents  = 1.1 * (box.bMax - box.bMin);
 
+    // Use Kokkos views to transfer r-o data from CPU to GPU
+    // KdTreeView treeview (&tree, KOKKOS_STUFF);
+
     // Query at random places in the tree
     CartVect params;
     int is_inside  = 0;
     int num_inside = 0;
     EntityHandle elem;
+    // nearest neighbor interpolation
     PUSH_TIMER()
     for( int i = 0; i < num_queries; i++ )
     {
         pos  = box.bMin + CartVect( box_extents[0] * .01 * ( rand() % 100 ), box_extents[1] * .01 * ( rand() % 100 ),
                                    box_extents[2] * .01 * ( rand() % 100 ) );
+        // Query technically on a GPU datastructure here.
         rval = sl.locate_point( pos.array(), elem, params.array(), &is_inside, 0.0, 0.0 );MB_CHK_ERR( rval );
         if( is_inside ) num_inside++;
     }
@@ -140,3 +147,4 @@ int main( int argc, char** argv )
 
     return 0;
 }
+
