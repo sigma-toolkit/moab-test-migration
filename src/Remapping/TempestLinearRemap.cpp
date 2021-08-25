b5c02f813bc8b4d93d51da78a482fbae19ca6152
Iulian Grindeanu <iulian@mcs.anl.gov>
2021-08-25 17:21:49 -0500

    reformat output about sparse matrix measures

--- src/Remapping/TempestLinearRemap.cpp
+++ src/Remapping/TempestLinearRemap.cpp
@@ -325,34 +325,22 @@ void moab::TempestOnlineMap::copy_tempest_sparsemat_to_eigen3()
     m_weightMatrix.setFromTriplets( tripletList.begin(), tripletList.end() );
     m_weightMatrix.makeCompressed();
 
-    /*m_weightMatrix.reserve( locvals );
-    for( unsigned iv = 0; iv < locvals; iv++ )
-    {
-        // std::cout << "Row = " << row_ldofmap[lrows[iv]] << ", Col = " << col_ldofmap[lcols[iv]]
-        // << ", DATA = " << lvals[iv] << std::endl; std::cout << "Row = " << lrows[iv] << ", Col =
-        // " << lcols[iv] << ", DATA = " << lvals[iv] << std::endl;
-        m_weightMatrix.insert( lrows[iv], lcols[iv] ) = lvals[iv];
-    }
-
-    m_weightMatrix.makeCompressed();*/
     int nrows = m_weightMatrix.rows();         // Number of rows
     int ncols = m_weightMatrix.cols();         // Number of columns
     int NNZ = m_weightMatrix.nonZeros();     // Number of non zero values
 #ifdef MOAB_HAVE_MPI
     // find out min/max for NNZ, ncols, nrows
     // should work on std c++ 11
-    int arr3[3] = {NNZ, nrows, ncols};
-    int rarr3[3];
-    int mpierr = MPI_Reduce(arr3, rarr3, 3, MPI_INT, MPI_MIN, 0, m_pcomm->comm());
-    if (!rank)
-        std::cout << "MIN nr rows: " << rarr3 [1] << " cols: " << rarr3[2] << " non-zeros: " << rarr3[0] << "\n";
+    int arr3[6] = {NNZ, nrows, ncols, -NNZ, -nrows, -ncols};
+    int rarr3[6];
+    int mpierr = MPI_Reduce(arr3, rarr3, 6, MPI_INT, MPI_MIN, 0, m_pcomm->comm());
 
-    mpierr = MPI_Reduce(arr3, rarr3, 3, MPI_INT, MPI_MAX, 0, m_pcomm->comm());
+    int totalNNZ=0;
+    mpierr = MPI_Reduce(arr3, &totalNNZ, 1, MPI_INT, MPI_SUM, 0, m_pcomm->comm());
     if (!rank)
-        std::cout << "MAX nr rows: " << rarr3 [1] << " cols: " << rarr3[2] << " non-zeros: " << rarr3[0] << "\n";
-    mpierr = MPI_Reduce(arr3, rarr3, 1, MPI_INT, MPI_SUM, 0, m_pcomm->comm());
-    if (!rank)
-        std::cout << " Total number of non-zeros: " << rarr3[0] << "\n";
+        std::cout << " Rows:(" << rarr3[1] <<", " <<  -rarr3[4] << "), Cols:(" <<
+                                  rarr3[2] <<", " <<  -rarr3[5] << "), NNZ:(" <<
+                                  rarr3[0] <<", " <<  -rarr3[3] << "),  total NNZ:" << totalNNZ << "\n";
 #else
     std::cout << "nr rows: " << nrows << " cols: " << ncols << " non-zeros: " << NNZ << > "\n";
 #endif
