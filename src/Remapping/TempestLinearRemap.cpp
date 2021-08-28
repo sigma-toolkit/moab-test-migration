11756d44b4a0fadd29dabb02310f2a71821bb4d3
Iulian Grindeanu <iulian@mcs.anl.gov>
2021-08-27 20:27:46 -0500

    fix reader on one task
    we do not need crystal router but we do need to
    instantiate correctly rowMap, colMap, row_gdofmap, col_gdofmap
    also, pay attention to the fact that file has 1 based indices for
    row and column, for sparse matrix
    internally, we use 0 based indices

--- src/Remapping/TempestLinearRemap.cpp
+++ src/Remapping/TempestLinearRemap.cpp
@@ -387,7 +387,7 @@ moab::ErrorCode moab::TempestOnlineMap::ApplyWeights( std::vector< double >& src
         // Permute the source data first
 #ifdef VERBOSE
         output_file << "ColVector: " << m_colVector.size() << ", SrcVals: " << srcVals.size()
-                    << ", Sizes: " << m_nTotDofs_SrcCov << ", " << col_gdofmap.size() << "\n";
+                    << ", Sizes: " << m_nTotDofs_SrcCov << ", " << col_dtoc_dofmap.size() << "\n";
 #endif
         for( unsigned i = 0; i < srcVals.size(); ++i )
         {
@@ -395,7 +395,7 @@ moab::ErrorCode moab::TempestOnlineMap::ApplyWeights( std::vector< double >& src
             {
                 m_colVector( col_dtoc_dofmap[i] ) = srcVals[i];  // permute and set the row (source) vector properly
 #ifdef VERBOSE
-                output_file << "Col: " << i << ", GID: " << col_gdofmap[i] << ", Data = " << srcVals[i] << "\n";
+                output_file << "Col: " << i << ", GID: " << col_dtoc_dofmap[i] << ", Data = " << srcVals[i] << "\n";
 #endif
             }
         }
