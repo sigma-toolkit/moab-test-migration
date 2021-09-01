5c07c10749e733676573de0b51586ea4695ff13b
Vijay Mahadevan <vijay.m@gmail.com>
2021-09-01 14:13:58 -0400

    Remove the moab_mpi_config autoconf generated header. This is redundant and should be stored in MOABConfig.h directly. Also src/moab/EntityHandle.hpp.in does not have any configure defines and so renaming it to just EntityHandle.hpp and including as regular header now.

--- src/parallel/moab_mpi.h
+++ src/parallel/moab_mpi.h
@@ -1,10 +1,10 @@
 #ifndef MOAB_MPI_H
 #define MOAB_MPI_H
-#include "moab_mpi_config.h"
+#include "moab/MOABConfig.h"
 
 #ifndef __cplusplus
 #  include <mpi.h>
-#elif !defined(MB_MPI_CXX_CONFLICT)
+#elif !defined(MOAB_MPI_CXX_CONFLICT)
 #  ifndef MPICH_IGNORE_CXX_SEEK
 #    define MPICH_IGNORE_CXX_SEEK
 #  endif
@@ -14,33 +14,33 @@
 #  ifdef SEEK_SET
 #    undef SEEK_SET
 #    ifdef MB_SEEK_SET
-#      define MB_RESTORE_SEEK_SET
+#      define MOAB_RESTORE_SEEK_SET
 #    endif
 #  endif
 #  ifdef SEEK_CUR
 #    undef SEEK_CUR
-#    ifdef MB_SEEK_CUR
-#      define MB_RESTORE_SEEK_CUR
+#    ifdef MOAB_SEEK_CUR
+#      define MOAB_RESTORE_SEEK_CUR
 #    endif
 #  endif
 #  ifdef SEEK_END
 #    undef SEEK_END
-#    ifdef MB_SEEK_END
-#      define MB_RESTORE_SEEK_END
+#    ifdef MOAB_SEEK_END
+#      define MOAB_RESTORE_SEEK_END
 #    endif
 #  endif
 #  include <mpi.h>
-#  ifdef MB_RESTORE_SEEK_SET
-#    undef MB_RESTORE_SEEK_SET
-#    define SEEK_SET MB_SEEK_SET
+#  ifdef MOAB_RESTORE_SEEK_SET
+#    undef MOAB_RESTORE_SEEK_SET
+#    define SEEK_SET MOAB_SEEK_SET
 #  endif
-#  ifdef MB_RESTORE_SEEK_CUR
-#    undef MB_RESTORE_SEEK_CUR
-#    define SEEK_CUR MB_SEEK_CUR
+#  ifdef MOAB_RESTORE_SEEK_CUR
+#    undef MOAB_RESTORE_SEEK_CUR
+#    define SEEK_CUR MOAB_SEEK_CUR
 #  endif
-#  ifdef MB_RESTORE_SEEK_END
-#    undef MB_RESTORE_SEEK_END
-#    define SEEK_END MB_SEEK_END
+#  ifdef MOAB_RESTORE_SEEK_END
+#    undef MOAB_RESTORE_SEEK_END
+#    define SEEK_END MOAB_SEEK_END
 #  endif
 #endif
 
