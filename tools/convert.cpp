e303e728c3aa6252dba0e4aa46fb9e09e4a331ba
Vijay Mahadevan <vijay.m@gmail.com>
2021-09-22 14:41:56 -0400

    Cleanup the DoF metadata assignment process for CS meshes

--- tools/convert.cpp
+++ tools/convert.cpp
@@ -159,10 +159,11 @@ static int process_partition_file( Interface* gMB, std::string& metis_partition_
 
 int main( int argc, char* argv[] )
 {
-    int proc_id = 0;
+    int proc_id = 0, nprocs = 1;
 #ifdef MOAB_HAVE_MPI
     MPI_Init( &argc, &argv );
     MPI_Comm_rank( MPI_COMM_WORLD, &proc_id );
+    MPI_Comm_size( MPI_COMM_WORLD, &nprocs );
 #endif
 
 #ifdef MOAB_HAVE_TEMPESTREMAP
