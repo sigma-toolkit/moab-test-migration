d7212c235fb98c597fab1c841829fab8d9f453dd
Iulian Grindeanu <iulian@mcs.anl.gov>
2022-02-02 11:12:34 -0600

    output always when nOverlap == 0

--- src/Remapping/TempestLinearRemap.cpp
+++ src/Remapping/TempestLinearRemap.cpp
@@ -183,8 +183,8 @@ void moab::TempestOnlineMap::LinearRemapFVtoFV_Tempest_MOAB( int nOrder )
 
         if( nOverlapFaces == 0 )
         {
-            if (rank > 997 &&  rank < 1005 )  {
-               std::cout <<"rank:" <<  rank <<  m_remapper->GetGlobalID( Remapper::CoveringMesh, ixFirst) << "\n";
+            {
+               std::cout <<"rank:" <<  rank << " " <<  m_remapper->GetGlobalID( Remapper::CoveringMesh, ixFirst) << "\n";
             }
             continue;
         }
