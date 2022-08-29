6c780a67764a9d3068c08ad747d2bc856ddd3292
Iulian Grindeanu <iulian@mcs.anl.gov>
2022-08-28 21:13:45 -0500

    option to set more tags at a time, for regular meshes too
    for example, use source and target meshes , more tags, like this
    mpiexec -np 2 ./imoab_setwithGid \
    -s /media/iulian/ExtraDrive1/output_E3SM/W44/run/o_flux_mct_00.h5m \
    -t /media/iulian/ExtraDrive1/output_E3SM/W44/run/o_flux.h5m \
    -g Faox_lat:Faox_lwup -e 1 -n 2
    
    ./imoab_setwithGid  -h
    Usage: imoab_setwithGid --help | [options]
    Options:
      -h [--help]       : Show full help text
      -s [--source] <arg>: mct mesh filename (source)
      -t [--target] <arg>: moab mesh filename (target)
      -g [--tags] <arg> : list of tags, colon separated
      -e [--enttype] <int>: entity type for moab mesh (0 point cloud, 1 cells)
      -n [--ntags] <int>: number of tags to set

--- test/parallel/imoab_setwithGid.cpp
+++ test/parallel/imoab_setwithGid.cpp
@@ -34,6 +34,7 @@ int main( int argc, char* argv[] )
     int rankInGlobalComm, numProcesses;
     MPI_Group jgroup;
     std::string readoptsLnd( "PARALLEL=READ_PART;PARTITION=PARALLEL_PARTITION" );
+    std::string readopts( "PARALLEL=READ_PART;PARTITION=PARALLEL_PARTITION;PARALLEL_RESOLVE_SHARED_ENTS" );
 
     // Timer data
     moab::CpuTimer timer;
@@ -51,25 +52,51 @@ int main( int argc, char* argv[] )
 
     std::string lndMoab = TestDir + "unittest/recLand.h5m";
     std::string lndMct = TestDir + "unittest/WHOLE_cx_lnd.h5m";
+    std::string tagname("frac");
+    int entType = 0;
+    int ntags = 1;
     int nghlay = 0; // no ghosts
+
     ierr = iMOAB_Initialize( argc, argv );  // not really needed anything from argc, argv, yet; maybe we should
     CHECKIERR( ierr, "Cannot initialize iMOAB" )
+
+    ProgOptions opts;
+    opts.addOpt< std::string >( "source,s", "mct mesh filename (source)", &lndMct );
+
+    opts.addOpt< std::string >( "target,t", "moab mesh filename (target)", &lndMoab );
+
+    opts.addOpt< std::string >( "tags,g", "list of tags, colon separated", &tagname );
+
+    opts.addOpt< int >( "enttype,e", "entity type for moab mesh (0 point cloud, 1 cells)", &entType );
+
+    opts.addOpt< int >( "ntags,n", "number of tags to set", &ntags );
+
+    opts.parseCommandLine( argc, argv );
+
     int cplLndAppID = -1, cplLnd2AppID = -1;   // -1 means it is not initialized
     iMOAB_AppID cplLndPID    = &cplLndAppID;     // land on coupler PEs, moab dist
     iMOAB_AppID cplLnd2PID    = &cplLnd2AppID;     // land on coupler PEs, MCT dist
     int cpllnd = 9, cpllnd2 = 109;
     ierr = iMOAB_RegisterApplication( "LNDX", &dup_comm_world, &cpllnd,
-                                              cplLndPID );  // lnd on coupler pes
+                                              cplLndPID );  // mesh on coupler pes
     CHECKIERR( ierr, "Cannot register LNDX over coupler PEs" )
     ierr = iMOAB_RegisterApplication( "LNDX2", &dup_comm_world, &cpllnd2,
-                                                  cplLnd2PID );  // lnd on coupler pes, MCT distr
+                                                  cplLnd2PID );  // mesh on coupler pes, MCT distr, point cloud
     CHECKIERR( ierr, "Cannot register LNDX2 over coupler PEs" )
 
-    ierr = iMOAB_LoadMesh( cplLndPID, lndMoab.c_str(), readoptsLnd.c_str(), &nghlay );
-    CHECKIERR( ierr, "Cannot load lnd on coupler pes" )
+    if (entType == 1)
+    {
+        ierr = iMOAB_LoadMesh( cplLndPID, lndMoab.c_str(), readopts.c_str(), &nghlay ); // moab mesh can be cells
+    }
+    else // entType == 0
+    {
+        ierr = iMOAB_LoadMesh( cplLndPID, lndMoab.c_str(), readoptsLnd.c_str(), &nghlay ); // moab mesh is point cloud
+    }
+
+    CHECKIERR( ierr, "Cannot load moab mesh on coupler pes" )
 
     ierr = iMOAB_LoadMesh( cplLnd2PID, lndMct.c_str(), readoptsLnd.c_str(), &nghlay );
-    CHECKIERR( ierr, "Cannot load lnd2 on coupler pes" )
+    CHECKIERR( ierr, "Cannot load mct point cloud on coupler pes" )
 
     int nverts[3], nelem[3];
     int tagType[2] = {  DENSE_DOUBLE, DENSE_INTEGER};
@@ -82,25 +109,28 @@ int main( int argc, char* argv[] )
 	 * returned in size 3 arrays, for local, ghost and total numbers.
 	 */
 	ierr = iMOAB_GetMeshInfo( cplLnd2PID, nverts, nelem, 0, 0, 0 );
-	CHECKIERR( ierr, "Cannot get info on lnd2 on coupler pes" )
+	CHECKIERR( ierr, "Cannot get info on mct mesh on coupler pes" )
 	std::vector<int> gids(nverts[0]);
-	std::vector<double> fracts(nverts[0]);
-	ierr = iMOAB_DefineTagStorage( cplLnd2PID, "frac", &tagType[0], &sizeTag, &tagIndex );
+	int nvals = nverts[0]*ntags;
+	std::vector<double> fracts(nvals);
+	ierr = iMOAB_DefineTagStorage( cplLnd2PID, tagname.c_str(), &tagType[0], &sizeTag, &tagIndex );
 	CHECKIERR( ierr, "Cannot define frac tag" )
 
 	ierr = iMOAB_DefineTagStorage( cplLnd2PID, "GLOBAL_ID", &tagType[1], &sizeTag, &tagIndex );
     CHECKIERR( ierr, "Cannot define gid tag" )
 
-	int entType = 0; // vertex
-	ierr  = iMOAB_GetDoubleTagStorage( cplLnd2PID, "frac", &nverts[0], &entType, &fracts[0] );
+	int mctEntType = 0; // vertex
+	ierr  = iMOAB_GetDoubleTagStorage( cplLnd2PID, tagname.c_str(), &nvals, &mctEntType, &fracts[0] );
 	CHECKIERR( ierr, "Cannot get frac tag on lnd2 on coupler pes" )
 
-	ierr  = iMOAB_GetIntTagStorage( cplLnd2PID, "GLOBAL_ID", &nverts[0], &entType, &gids[0] );
+	ierr  = iMOAB_GetIntTagStorage( cplLnd2PID, "GLOBAL_ID", &nverts[0], &mctEntType, &gids[0] );
     CHECKIERR( ierr, "Cannot get global id tag on lnd2 on coupler pes" )
 
-	ierr = iMOAB_DefineTagStorage( cplLndPID, "frac", &tagType[0], &sizeTag, &tagIndex );
-    CHECKIERR( ierr, "Cannot define frac tag" )
-	ierr = iMOAB_SetDoubleTagStorageWithGid(cplLndPID, "frac", &nverts[0], &entType, &fracts[0], &gids[0] );
+	// now on moab mesh
+	ierr = iMOAB_DefineTagStorage( cplLndPID, tagname.c_str(), &tagType[0], &sizeTag, &tagIndex );
+    CHECKIERR( ierr, "Cannot define list of tags" )
+
+	ierr = iMOAB_SetDoubleTagStorageWithGid(cplLndPID, tagname.c_str(), &nvals, &entType, &fracts[0], &gids[0] );
     CHECKIERR( ierr, "Cannot set double tag with global ids " )
 
     char fileWriteOptions[] = "PARALLEL=WRITE_PART";
