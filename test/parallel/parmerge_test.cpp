bd1ffa4c9ab485167475248f35a48306e2980663
Iulian Grindeanu <iulian@mcs.anl.gov>
2022-11-21 10:25:04 -0600

    make parmerge_test more interesting
    also, make it run on arbitrary number of processes
    load on each task one of the 2 bricks (0 or 1), and translate
    them in z direction by rank/2 * 10.
    (10 is the size of each brick)
    parallel merge should merge all these bricks nicely

--- test/parallel/parmerge_test.cpp
+++ test/parallel/parmerge_test.cpp
@@ -14,13 +14,6 @@ int main( int argc, char* argv[] )
     MPI_Comm_size( MPI_COMM_WORLD, &nproc );
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 
-    if( nproc != 2 )
-    {
-        std::cout << " run this test on 2 tasks\n";
-        MPI_Finalize();
-        return 1;
-    }
-
     std::string filename0 = TestDir + "unittest/brick1.vtk";
     std::string filename1 = TestDir + "unittest/brick2.vtk";
 
@@ -28,7 +21,7 @@ int main( int argc, char* argv[] )
     moab::ParallelComm* pc = new moab::ParallelComm( mb, MPI_COMM_WORLD );
     ErrorCode rval         = MB_SUCCESS;
 
-    if( 0 == rank )
+    if( 0 == rank%2 )
         rval = mb->load_file( filename0.c_str() );
     else
         rval = mb->load_file( filename1.c_str() );
@@ -41,6 +34,23 @@ int main( int argc, char* argv[] )
         MPI_Finalize();
         return 1;
     }
+    // if rank > 2, translate in z direction, with the distance ( (rank)/2 ) * 10
+    if (rank >= 2)
+    {
+        Range verts;
+        rval = mb->get_entities_by_type(0, MBVERTEX, verts);MB_CHK_ERR( rval );
+        int num_verts=(int)verts.size();
+        std::vector<double> coords;
+        coords.resize(num_verts*3);
+        rval = mb->get_coords(verts, &coords[0]);MB_CHK_ERR( rval );
+        int steps=rank/2;
+        double z_translate = steps*10.;// the 2 bricks are size 10
+        for (int i=0; i<num_verts; i++)
+            coords[3*i+2] += z_translate;
+        rval = mb->set_coords(verts, &coords[0]);MB_CHK_ERR( rval );
+    }
+
+
     ParallelMergeMesh pm( pc, 0.001 );
     rval = pm.merge();
     if( rval != MB_SUCCESS )
@@ -51,32 +61,34 @@ int main( int argc, char* argv[] )
         MPI_Finalize();
         return 1;
     }
-    // check number of shared entities
-    Range shared_ents;
-    // Get entities shared with all other processors
-    rval = pc->get_shared_entities( -1, shared_ents );
-    if( rval != MB_SUCCESS )
+    if (nproc == 2)
     {
-        delete pc;
-        delete mb;
-        MPI_Finalize();
-        return 1;
-    }
-    // there should be exactly 9 vertices, 12 edges, 4 faces
-    unsigned numV = shared_ents.num_of_dimension( 0 );
-    unsigned numE = shared_ents.num_of_dimension( 1 );
-    unsigned numF = shared_ents.num_of_dimension( 2 );
+        // check number of shared entities
+        Range shared_ents;
+        // Get entities shared with all other processors
+        rval = pc->get_shared_entities( -1, shared_ents );
+        if( rval != MB_SUCCESS )
+        {
+            delete pc;
+            delete mb;
+            MPI_Finalize();
+            return 1;
+        }
+        // there should be exactly 9 vertices, 12 edges, 4 faces
+        unsigned numV = shared_ents.num_of_dimension( 0 );
+        unsigned numE = shared_ents.num_of_dimension( 1 );
+        unsigned numF = shared_ents.num_of_dimension( 2 );
 
-    if( numV != 9 || numE != 12 || numF != 4 )
-    {
-        std::cout << " wrong number of shared entities on proc " << rank << " v:" << numV << " e:" << numE
-                  << " f:" << numF << "\n";
-        delete pc;
-        delete mb;
-        MPI_Finalize();
-        return 1;
+        if( numV != 9 || numE != 12 || numF != 4 )
+        {
+            std::cout << " wrong number of shared entities on proc " << rank << " v:" << numV << " e:" << numE
+                      << " f:" << numF << "\n";
+            delete pc;
+            delete mb;
+            MPI_Finalize();
+            return 1;
+        }
     }
-
     rval = mb->write_file( "testpm.h5m", 0, "PARALLEL=WRITE_PART" );
     if( rval != MB_SUCCESS )
     {
