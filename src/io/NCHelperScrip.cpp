dbde40438e4570414290ec9a13db4964c0cf10ed
Iulian Grindeanu <iulian@mcs.anl.gov>
2023-02-07 16:59:20 -0600

    use macros for error check

--- src/io/NCHelperScrip.cpp
+++ src/io/NCHelperScrip.cpp
@@ -363,8 +363,7 @@ ErrorCode NCHelperScrip::create_mesh( Range& faces )
             EntityHandle eh          = *it;
             const EntityHandle* conn = NULL;
             int num_nodes            = 0;
-            rval                     = mb->get_connectivity( eh, conn, num_nodes );
-            if( MB_SUCCESS != rval ) return rval;
+            rval                     = mb->get_connectivity( eh, conn, num_nodes );MB_CHK_ERR( rval );
             adj_fact->notify_create_entity( eh, conn, num_nodes );
         }
     }
