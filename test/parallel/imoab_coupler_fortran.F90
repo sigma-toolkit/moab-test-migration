523f5224a33e7a5c4f4278af7469ee519a79ceb2
Vijay Mahadevan <vijay.m@gmail.com>
2023-03-21 12:48:13 -0400

    Add bilinear map generation to a fortran test

--- test/parallel/imoab_coupler_fortran.F90
+++ test/parallel/imoab_coupler_fortran.F90
@@ -222,8 +222,14 @@ program imoab_coupler_fortran
 
       ierr = iMOAB_ComputeScalarProjectionWeights( &
              cplAtmOcnPID, weights_identifier1, disc_methods1, disc_orders1, &
-             disc_methods2, disc_orders2, C_NULL_CHAR, fNoBubble, fMonotoneTypeID, fVolumetric, fInverseDistanceMap, fNoConserve, &
+             disc_methods2, disc_orders2, ""//C_NULL_CHAR, fNoBubble, fMonotoneTypeID, fVolumetric, fInverseDistanceMap, fNoConserve, &
              fValidate, dof_tag_names1, dof_tag_names2)
+      call errorout(ierr, 'cannot compute scalar projection weights')
+
+      ierr = iMOAB_ComputeScalarProjectionWeights( &
+             cplAtmOcnPID, "bilinear"//C_NULL_CHAR, "fv"//C_NULL_CHAR, 1, &
+             "fv"//C_NULL_CHAR, 1, "bilin"//C_NULL_CHAR, fNoBubble, fMonotoneTypeID, fVolumetric, fInverseDistanceMap, fNoConserve, &
+             fValidate, "GLOBAL_ID"//C_NULL_CHAR, "GLOBAL_ID"//C_NULL_CHAR)
       call errorout(ierr, 'cannot compute scalar projection weights')
 
 #ifdef MOAB_HAVE_NETCDF
