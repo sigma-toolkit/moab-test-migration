4091668a1a81cfbca2105608183bb4a9ba7fda23
Vijay Mahadevan <vijay.m@gmail.com>
2023-03-27 10:15:47 -0500

    The box tolerance needs to be coarse to allow for more layers needed for bilinear and high-order maps in parallel

--- src/iMOAB.cpp
+++ src/iMOAB.cpp
@@ -3978,7 +3978,7 @@ ErrCode iMOAB_ComputeMeshIntersectionOnSphere( iMOAB_AppID pid_src, iMOAB_AppID 
     double radius_source = 1.0;
     double radius_target = 1.0;
     const double epsrel  = ReferenceTolerance;  // ReferenceTolerance is defined in Defines.h in tempestremap source ;
-    const double boxeps  = 1.e-6;
+    const double boxeps  = 1.e-1;
 
     // Get the source and target data and pcomm objects
     appData& data_src  = context.appDatas[*pid_src];
