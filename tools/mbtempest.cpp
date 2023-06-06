ff6e5925430ecba5ecc0de9491156eb8c737180d
Iulian Grindeanu <iulian@mcs.anl.gov>
2023-06-06 17:13:31 -0500

    add gnomonic option to mbtempest
    if bilinear, do not use gnomonic

--- tools/mbtempest.cpp
+++ tools/mbtempest.cpp
@@ -569,7 +569,10 @@ int main( int argc, char* argv[] )
         // First compute the covering set such that the target elements are fully covered by the
         // lcoal source grid
         runCtx->timer_push( "construct covering set for intersection" );
-        rval = remapper.ConstructCoveringSet( epsrel, 1.0, 1.0, boxeps, runCtx->rrmGrids );MB_CHK_ERR( rval );
+	bool gnomonic = true;
+	if (runCtx->fvMethod == std::string("bilin"))
+		gnomonic = false;
+        rval = remapper.ConstructCoveringSet( epsrel, 1.0, 1.0, boxeps, runCtx->rrmGrids, gnomonic );MB_CHK_ERR( rval );
         runCtx->timer_pop();
 
         // Compute intersections with MOAB with either the Kd-tree or the advancing front algorithm
