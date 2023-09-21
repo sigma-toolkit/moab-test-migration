186ca2956dcea3085ec915885b0ca20e82343c57
Vijay Mahadevan <vijay.m@gmail.com>
2023-09-21 10:26:22 -0400

    Some significant updates on layout, UI and some content to make the experience better

---
+++ doc/details/e_moabteam.h
@@ -0,0 +1,24 @@
+/*! \page team MOAB team members
+
+ <h2>Current Team: </h2>
+ 
+ - Vijay S. Mahadevan (Argonne National Lab)
+ - Iulian Grindeanu (Argonne National Lab) 
+ - Rajeev Jain (Argonne National Lab)
+ - Paul Wilson (Univ Wisconsin-Madison)
+ - Patrick Shriwise (Argonne National Lab)
+
+ <h2>Emeritus members:</h2>
+ 
+ - Timothy J. Tautges (Siemens, Univ Wisconsin-Madison)
+ - Danqing Wu  (Argonne National Lab)
+ - Navamita Ray (Los Alamos National Lab)
+ - Jane Hu (Univ Wisconsin-Madison)
+ - Anthony Scopatz (The University of South Carolina)
+ - Jason A. Kraftcheck
+ - Brandon M. Smith
+ - Hong-Jun Kim
+ - Jim Porter
+ - Xiabing Xu
+ 
+*/
