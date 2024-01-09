1e8bf4b48702d0fc2cf62c868de84e931008850c
Iulian Grindeanu <iulian@mcs.anl.gov>
2024-01-08 20:39:50 -0600

    set with gid should work for culled meshes
    when we set by gid, we match by the global id on both sides
    sometimes the entity with a global id does not exist, skip
    the setting in that case

--- test/parallel/imoab_setwithGid.cpp
+++ test/parallel/imoab_setwithGid.cpp
@@ -63,7 +63,10 @@ int main( int argc, char* argv[] )
 
     opts.addOpt< std::string >( "tags,g", "list of tags, colon separated", &tagname );
 
-    opts.addOpt< int >( "enttype,e", "entity type for moab mesh (0 point cloud, 1 cells)", &entType );
+    opts.addOpt< int >( "enttype,e", "entity type for source mesh (0 point cloud, 1 cells)", &entType );
+    int mctEntType = 0;  // vertex
+
+    opts.addOpt< int >( "tenttype,m", "entity type for target mesh (0 point cloud, 1 cells)", &mctEntType );
 
     opts.addOpt< int >( "ntags,n", "number of tags to set", &ntags );
 
@@ -71,7 +74,7 @@ int main( int argc, char* argv[] )
 
     int cplLndAppID = -1, cplLnd2AppID = -1;  // -1 means it is not initialized
     iMOAB_AppID cplLndPID  = &cplLndAppID;    // land on coupler PEs, moab dist
-    iMOAB_AppID cplLnd2PID = &cplLnd2AppID;   // land on coupler PEs, MCT dist
+    iMOAB_AppID cplLnd2PID = &cplLnd2AppID;   // land on coupler PEs, another dist
     int cpllnd = 9, cpllnd2 = 109;
     ierr = iMOAB_RegisterApplication( "LNDX", &dup_comm_world, &cpllnd,
                                       cplLndPID );  // mesh on coupler pes
@@ -91,8 +94,15 @@ int main( int argc, char* argv[] )
 
     CHECKIERR( ierr, "Cannot load moab mesh on coupler pes" )
 
-    ierr = iMOAB_LoadMesh( cplLnd2PID, lndMct.c_str(), readoptsLnd.c_str(), &nghlay );
-    CHECKIERR( ierr, "Cannot load mct point cloud on coupler pes" )
+    if (mctEntType == 0)
+    {
+       ierr = iMOAB_LoadMesh( cplLnd2PID, lndMct.c_str(), readoptsLnd.c_str(), &nghlay );
+    }
+    else
+    {
+        ierr = iMOAB_LoadMesh( cplLnd2PID, lndMct.c_str(), readopts.c_str(), &nghlay );
+    }
+    CHECKIERR( ierr, "Cannot load second mesh on coupler pes" )
 
     int nverts[3], nelem[3];
     int tagType[2] = { DENSE_DOUBLE, DENSE_INTEGER };
@@ -108,6 +118,8 @@ int main( int argc, char* argv[] )
     CHECKIERR( ierr, "Cannot get info on mct mesh on coupler pes" )
     std::vector< int > gids( nverts[0] );
     int nvals = nverts[0] * ntags;
+    if (mctEntType == 1)
+        nvals = nelem[0] * ntags;
     std::vector< double > fracts( nvals );
     ierr = iMOAB_DefineTagStorage( cplLnd2PID, tagname.c_str(), &tagType[0], &sizeTag, &tagIndex );
     CHECKIERR( ierr, "Cannot define frac tag" )
@@ -115,11 +127,13 @@ int main( int argc, char* argv[] )
     ierr = iMOAB_DefineTagStorage( cplLnd2PID, "GLOBAL_ID", &tagType[1], &sizeTag, &tagIndex );
     CHECKIERR( ierr, "Cannot define gid tag" )
 
-    int mctEntType = 0;  // vertex
     ierr           = iMOAB_GetDoubleTagStorage( cplLnd2PID, tagname.c_str(), &nvals, &mctEntType, &fracts[0] );
-    CHECKIERR( ierr, "Cannot get frac tag on lnd2 on coupler pes" )
+    CHECKIERR( ierr, "Cannot get tag on lnd2 on coupler pes" )
 
-    ierr = iMOAB_GetIntTagStorage( cplLnd2PID, "GLOBAL_ID", &nverts[0], &mctEntType, &gids[0] );
+    int ngids = nverts[0];
+    if (mctEntType == 1)
+        ngids = nelem[0];
+    ierr = iMOAB_GetIntTagStorage( cplLnd2PID, "GLOBAL_ID", &ngids, &mctEntType, &gids[0] );
     CHECKIERR( ierr, "Cannot get global id tag on lnd2 on coupler pes" )
 
     // now on moab mesh
