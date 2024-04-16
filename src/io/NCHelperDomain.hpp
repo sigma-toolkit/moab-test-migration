70e6ea530adb8a8912d2851d7b73774f1fe5f916
Iulian Grindeanu <iulian@mcs.anl.gov>
2024-04-16 16:28:45 -0500

    add a new method for zoltan partitioner
    eventually, we will use this one for mpas too, it
    would be easier to do a trivial read, and then repartition
    in memory, compared to doing a scattered read with pnetcdf,
    which is more complex

--- src/io/NCHelperDomain.hpp
+++ src/io/NCHelperDomain.hpp
@@ -11,7 +11,9 @@
 
 namespace moab
 {
-
+#ifdef MOAB_HAVE_MPI
+class ParallelComm;
+#endif
 //! Child helper class for Domain grid
 class NCHelperDomain : public ScdNCHelper
 {
@@ -30,7 +32,17 @@ class NCHelperDomain : public ScdNCHelper
     {
         return "DOMAIN";
     }
-
+#ifdef MOAB_HAVE_MPI
+    ErrorCode redistribute_cells( moab::ParallelComm * myPcomm,
+                                  std::vector<double> & xc,
+                                  std::vector<double> & yc,
+                                  std::vector<double> & xv,
+                                  std::vector<double> & yv,
+                                  std::vector<double> & frac,
+                                  std::vector<int> & mask,
+                                  std::vector<double> & area,
+                                  std::vector<int> & gids );
+#endif
     int nv;     // number of vertices per cell
     int nvDim;  // index of nv dim
 };
