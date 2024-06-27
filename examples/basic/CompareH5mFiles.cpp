c9cdd55a4abc68fabfdbe43a2a744f7501bf130f
Iulian Grindeanu <iulian@mcs.anl.gov>
2024-06-27 14:24:53 -0500

    add option -G to CompareH5mFiles
    this will allow to compare files that have the cells in different order,
    due maybe to different partitioning
    for example, run ./moab_coupler_caas on 1 or 2 processes, you can use
    this to compare the results:
    CompareH5mFiles -f OcnOnCplF_n02.h5m -g  OcnOnCplF_n01.h5m -G
     compare files: OcnOnCplF_n02.h5m and OcnOnCplF_n01.h5m dimension entity: 2
     tag: Sa_dens_bilin_proj 	 difference : 3.35287e-14 	 min/max (1.08409/1.62956) 	 (1.08409/1.62956)
     tag: Sa_dens_o2_caas_proj 	 difference : 7.81597e-13 	 min/max (1.06083/1.59316) 	 (1.06083/1.59316)
     tag: Sa_dens_o2_proj 	 difference : 7.69829e-13 	 min/max (0.916089/3.4716) 	 (0.916089/3.4716)
     tag: Sa_dens_proj 	 difference : 1.15463e-14 	 min/max (1.07548/1.63437) 	 (1.07548/1.63437)
     tag: Sa_pbot_bilin_proj 	 difference : 2.56114e-09 	 min/max (82700.6/102535) 	 (82700.6/102535)
     tag: Sa_pbot_o2_caas_proj 	 difference : 6.31262e-08 	 min/max (80388.4/102177) 	 (80388.4/102177)
     tag: Sa_pbot_o2_proj 	 difference : 6.14818e-08 	 min/max (76454/234140) 	 (76454/234140)
     tag: Sa_pbot_proj 	 difference : 9.45874e-10 	 min/max (81407.2/102564) 	 (81407.2/102564)
     different fields:8
     exactly the same fields:0

--- examples/basic/CompareH5mFiles.cpp
+++ examples/basic/CompareH5mFiles.cpp
@@ -27,7 +27,12 @@ int main( int argc, char** argv )
     opts.addOpt< std::string >( "file1,f", "first file", &file1 );
     opts.addOpt< std::string >( "file2,g", "second file", &file2 );
 
+    opts.addOpt<void>( "global,G", "use global id for correspondence" );
+
+
     opts.parseCommandLine( argc, argv );
+
+    bool use_global_id = opts.numOptSet("global") > 0;
 
     ErrorCode rval;
     Core* mb = new Core();
@@ -40,6 +45,9 @@ int main( int argc, char** argv )
     std::vector< Tag > list1;
     rval = mb->tag_get_tags( list1 );MB_CHK_SET_ERR( rval, "can't get tags 1" );
 
+    Tag gidTag = mb->globalId_tag();
+    Tag gidTag2 = mb2->globalId_tag();
+
     Range cells1;
     rval = mb->get_entities_by_dimension( 0, dim, cells1 );MB_CHK_SET_ERR( rval, "can't get cells 1" );
     if( cells1.size() == 0 )
@@ -48,33 +56,34 @@ int main( int argc, char** argv )
         rval = mb->get_entities_by_dimension( 0, dim, cells1 );MB_CHK_SET_ERR( rval, "can't get cells 1" );
     }
 
+
     Range cells2;
     rval = mb2->get_entities_by_dimension( 0, dim, cells2 );MB_CHK_SET_ERR( rval, "can't get cells 2" );
 
     if( cells1.size() != cells2.size() ) MB_CHK_SET_ERR( MB_FAILURE, "different size models " );
-
-    Tag tagid1 = mb->globalId_tag();
-    Tag tagid2 = mb2->globalId_tag();
-    vector< int > ids1, ids2;
-    ids1.resize( cells1.size() );
-    ids2.resize( cells2.size() );
-    rval = mb->tag_get_data( tagid1, cells1, &ids1[0] );MB_CHK_SET_ERR( rval, "can't get global ids on model 1" );
-    rval = mb2->tag_get_data( tagid2, cells2, &ids2[0] );MB_CHK_SET_ERR( rval, "can't get global ids on model 2" );
-
     vector< double > vals1, vals2;
     vals1.resize( cells1.size() );
     vals2.resize( cells2.size() );
 
-    std::map<int, int> idMap1;
-    for (size_t i=0; i<ids1.size(); i++)
+    std::vector<int> gids, gids2;
+    std::map<int, int> gidMap2;
+    if (use_global_id)
     {
-        idMap1[ids1[i]] = i;
+        gids.resize(cells1.size());
+        rval = mb->tag_get_data( gidTag, cells1, &gids[0] );MB_CHK_SET_ERR( rval, "can't get ids for cells1" );
+        gids2.resize(cells2.size());
+        rval = mb2->tag_get_data( gidTag2, cells2, &gids2[0] );MB_CHK_SET_ERR( rval, "can't get ids for cells2" );
+        for (int i=0; i<cells2.size(); i++)
+        {
+            gidMap2[ gids2[i] ] = i;
+        }
     }
 
     int k  = 0;  // number of different fields
     int k1 = 0;  // number of exactly the same fields
     std::cout << " compare files: " << file1 << " and " << file2 << " dimension entity: " << dim << "\n";
     std::vector< std::string > same_fields;
+    std::vector<Tag> diffTags;
     for( size_t i = 0; i < list1.size(); i++ )
     {
         Tag tag = list1[i];
@@ -91,8 +100,7 @@ int main( int argc, char** argv )
         if( 1 != length ) continue;
         Tag tag2;
         rval = mb2->tag_get_handle( name.c_str(), tag2 );MB_CHK_SET_ERR( rval, "can't get tag on second model" );
-        rval = mb->tag_get_data( tag, cells1, &vals1[0] );
-        if (MB_SUCCESS != rval) continue; // do not bother with tags without values	
+        rval = mb->tag_get_data( tag, cells1, &vals1[0] );MB_CHK_SET_ERR( rval, "can't get values on tag on model 1" );
         rval = mb2->tag_get_data( tag2, cells2, &vals2[0] );MB_CHK_SET_ERR( rval, "can't get values on tag on model 2" );
         double minv1, maxv1, minv2, maxv2;
         if( vals1.size() > 0 )
@@ -104,28 +112,66 @@ int main( int argc, char** argv )
             minv2 = maxv2 = vals2[0];
         }
         // compute the difference
-        double sum = 0;
+        double sum = 0, value1, value2;
         for( int j = 0; j < vals1.size(); j++ )
         {
-            int gid2 = ids2[j];
-            int index1 = idMap1[gid2];
-            sum += fabs( vals1[index1] - vals2[j] );
-            if( vals1[index1] < minv1 ) minv1 = vals1[index1];
-            if( vals1[index1] > maxv1 ) maxv1 = vals1[index1];
-            if( vals2[j] < minv2 ) minv2 = vals2[j];
-            if( vals2[j] > maxv2 ) maxv2 = vals2[j];
+            value1 = vals1[j];
+            if (use_global_id)
+            {
+
+                int index2 = gidMap2[ gids[j] ];
+                value2 = vals2[index2];
+
+            }
+            else
+            {
+                value2 = vals2[j];
+
+
+            }
+            sum += fabs( value1 - value2 );
+            if( value1 < minv1 ) minv1 = value1;
+            if( value1 > maxv1 ) maxv1 = value1;
+            if( value2 < minv2 ) minv2 = value2;
+            if( value2 > maxv2 ) maxv2 = value2;
         }
+
         if( sum > 0. )
         {
             std::cout << " tag: " << name << " \t difference : " << sum << " \t min/max (" << minv1 << "/" << maxv1
                       << ") \t (" << minv2 << "/" << maxv2 << ") \n";
             k++;
+            if (use_global_id)
+            {
+                for (int j=0; j< vals1.size(); j++)
+                {
+                    int index2 = gidMap2[ gids[j] ];
+                    vals1[j] -= vals2[index2];
+                }
+            }
+            else
+            {
+                for (int j=0; j< vals1.size(); j++)
+                {
+                    vals1[j] -= vals2[j];
+                }
+            }
+            std::string diffTagName = name+"_diff";
+            Tag newTag;
+            rval = mb->tag_get_handle( diffTagName.c_str(), 1, MB_TYPE_DOUBLE, newTag, MB_TAG_CREAT | MB_TAG_DENSE );MB_CHK_ERR( rval );
+            rval = mb->tag_set_data(newTag, cells1, &vals1[0]);MB_CHK_ERR( rval );
+            diffTags.push_back(newTag);
+
         }
         else
         {
             same_fields.push_back( name );
             k1++;
         }
+    }
+    if (k>0)
+    {
+        rval = mb->write_file("diff_tags.h5m", 0, 0, 0, 0, &diffTags[0], diffTags.size() ); MB_CHK_ERR( rval );
     }
     std::cout << " different fields:" << k << " \n exactly the same fields:" << k1 << "\n";
     for( size_t i = 0; i < same_fields.size(); i++ )
