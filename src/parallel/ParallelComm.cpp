63abd7cb52ac836b166c9e90422f9de524c024c3
Iulian Grindeanu <iulian@mcs.anl.gov>
2024-07-01 16:55:55 -0500

    comment out the sorting of new_ents after unpacking
    my guess is that is the problem
    this would be really terrible

--- src/parallel/ParallelComm.cpp
+++ src/parallel/ParallelComm.cpp
@@ -1851,7 +1851,7 @@ ErrorCode ParallelComm::pack_entity_seq( const int nodes_per_entity,
 
     // Pack the nodes per entity
     PACK_INT( buff->buff_ptr, nodes_per_entity );
-    myDebug->tprintf( 3, "after some pack int  %d \n", buff->get_current_size() );
+    myDebug->tprintf( 3, "pack seq start, buff size:  %d \n", buff->get_current_size() );
 
     // Pack the connectivity
     std::vector< EntityHandle > connect;
@@ -2352,7 +2352,7 @@ ErrorCode ParallelComm::unpack_entities( unsigned char*& buff_ptr,
     // Need to sort here, to enable searching
     // keep a deep copy order of entities created
     original_new_ents = new_ents;
-    std::sort( new_ents.begin(), new_ents.end() );
+    //std::sort( new_ents.begin(), new_ents.end() );
 
     return MB_SUCCESS;
 }
@@ -3445,7 +3445,7 @@ ErrorCode ParallelComm::unpack_sets( unsigned char*& buff_ptr,
         result = update_remote_data( new_sets, dum_range, from_proc, 0 );MB_CHK_SET_ERR( result, "Failed to set sharing data for sets" );
     }
 
-    myDebug->tprintf( 4, "Done unpacking sets." );
+    myDebug->tprintf( 4, "Done unpacking sets.\n" );
 
     return MB_SUCCESS;
 }
@@ -3506,7 +3506,7 @@ ErrorCode ParallelComm::pack_tags( Range& entities,
         if( MB_SUCCESS != result ) return result;
     }
 
-    myDebug->tprintf( 4, "Done packing tags." );
+    myDebug->tprintf( 4, "Done packing tags.\n" );
 
     buff->set_stored_size();
 
