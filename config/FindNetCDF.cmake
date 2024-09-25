127476f2f7eb04df9cc3865c63ca5542fc4d59ac
Vijay Mahadevan <vijay.m@gmail.com>
2024-09-25 11:13:27 -0400

    Fix several inconsistencies and remove the custom FindHDF5_MOAB

--- config/FindNetCDF.cmake
+++ config/FindNetCDF.cmake
@@ -5,71 +5,66 @@
 # NETCDF_LIBRARIES           - list of libraries to link against when using NetCDF
 # NETCDF_FOUND               - Do not attempt to use NetCDF if "no", "0", or undefined.
 
-set (NETCDF_ROOT "/usr" CACHE PATH "Path to search for NetCDF header and library files" )
+set (NETCDF_DIR "/usr" CACHE PATH "Path to search for NetCDF header and library files" )
 set (NETCDF_FOUND NO CACHE INTERNAL "Found NetCDF components successfully." )
 
   find_path( NETCDF_INCLUDE_DIR netcdf.h
-    HINTS ${NETCDF_ROOT}
-    ${NETCDF_ROOT}/include
+    HINTS ${NETCDF_DIR}
+    ${NETCDF_DIR}/include
     ENV CPLUS_INCLUDE_PATH
     NO_DEFAULT_PATH
   )
 
 find_library( NETCDF_C_LIBRARY
     NAMES netcdf libnetcdf.a
-    HINTS ${NETCDF_ROOT}
-    ${NETCDF_ROOT}/lib/x86_64-linux-gnu
-    ${NETCDF_ROOT}/lib64
-    ${NETCDF_ROOT}/lib
+    HINTS ${NETCDF_DIR}
+    ${NETCDF_DIR}/lib/x86_64-linux-gnu
+    ${NETCDF_DIR}/lib64
+    ${NETCDF_DIR}/lib
     NO_DEFAULT_PATH
   )
 
 find_library( NETCDF_CXX_LIBRARY
     NAMES netcdf_c++ libnetcdf_c++.a
-    HINTS ${NETCDF_ROOT}
-    ${NETCDF_ROOT}/lib/x86_64-linux-gnu
-    ${NETCDF_ROOT}/lib64
-    ${NETCDF_ROOT}/lib
+    HINTS ${NETCDF_DIR}
+    ${NETCDF_DIR}/lib/x86_64-linux-gnu
+    ${NETCDF_DIR}/lib64
+    ${NETCDF_DIR}/lib
     NO_DEFAULT_PATH
   )
 
 find_library( NETCDF_FORTRAN_LIBRARY
     NAMES netcdf_g77 netcdff netcdf_ifc netcdf_x86_64
-    HINTS ${NETCDF_ROOT}
-    ${NETCDF_ROOT}/lib/x86_64-linux-gnu
-    ${NETCDF_ROOT}/lib64
-    ${NETCDF_ROOT}/lib
+    HINTS ${NETCDF_DIR}
+    ${NETCDF_DIR}/lib/x86_64-linux-gnu
+    ${NETCDF_DIR}/lib64
+    ${NETCDF_DIR}/lib
     NO_DEFAULT_PATH
   )
 
   IF (NOT NETCDF_FOUND)
     if ( NETCDF_INCLUDE_DIR AND NETCDF_C_LIBRARY )
       set(NETCDF_FOUND YES )
-      set(NETCDF_INCLUDES "${NETCDF_INCLUDE_DIR}")
+      set(NETCDF_INCLUDES ${NETCDF_INCLUDE_DIR})
       set(NETCDF_LIBRARIES ${NETCDF_C_LIBRARY})
       if ( NETCDF_CXX_LIBRARY )
-        set(NETCDF_LIBRARIES ${NETCDF_LIBRARIES} ${NETCDF_CXX_LIBRARY})
+        set(NETCDF_LIBRARIES ${NETCDF_CXX_LIBRARY} ${NETCDF_LIBRARIES} )
       endif ( NETCDF_CXX_LIBRARY )
       if ( NETCDF_FORTRAN_LIBRARY )
-        set(NETCDF_LIBRARIES ${NETCDF_LIBRARIES} ${NETCDF_FORTRAN_LIBRARY})
+        set(NETCDF_LIBRARIES ${NETCDF_FORTRAN_LIBRARY} ${NETCDF_LIBRARIES} )
       endif ( NETCDF_FORTRAN_LIBRARY )
-      # print some details about NetCDF configuration
-      message (STATUS "---   NetCDF Configuration ::")
-      message (STATUS "        Directory : ${NETCDF_ROOT}")
-      message (STATUS "        INCLUDES  : ${NETCDF_INCLUDES}")
-      message (STATUS "        LIBRARIES : ${NETCDF_LIBRARIES}")
     else ( NETCDF_INCLUDE_DIR AND NETCDF_C_LIBRARY )
       set( NETCDF_FOUND NO )
-      message("finding NetCDF failed, please try to set the var NETCDF_ROOT")
+      message("finding NetCDF failed, please try to set the var NETCDF_DIR")
     endif ( NETCDF_INCLUDE_DIR AND NETCDF_C_LIBRARY )
   ENDIF (NOT NETCDF_FOUND)
 
 mark_as_advanced(
-  NETCDF_ROOT
+  NETCDF_DIR
   NETCDF_INCLUDES
   NETCDF_LIBRARIES
 )
 
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (NetCDF "NetCDF not found, check the CMake NETCDF_ROOT variable"
-  NETCDF_ROOT NETCDF_INCLUDES NETCDF_LIBRARIES)
+find_package_handle_standard_args (NetCDF "NetCDF not found, check the CMake NETCDF_DIR variable"
+  NETCDF_DIR NETCDF_INCLUDES NETCDF_LIBRARIES)
