d400193ce5b39b67b78b90735d4963b8e0c59f44
Vijay Mahadevan <vijay.m@gmail.com>
2024-10-10 22:36:23 -0400

    Replicate problem with loading domain file and trying to get ghost regions - but not getting any

--- test/parallel/imoab_exchange_tags.F90
+++ test/parallel/imoab_exchange_tags.F90
@@ -25,8 +25,7 @@
 
 #include "mpif.h"
 
-         integer ierr, my_id, num_procs, pid, i, ix, iy, numv, nume
-         integer dime, lco, mbtype, blockid, npe
+         integer ierr, rank, nprocs, pid
          integer compid
          character :: appname*10
          character(:), allocatable :: inputFileName
@@ -37,14 +36,21 @@
          integer, dimension(2) ::  entity_type
          integer, dimension(2) ::  tagIndex
          integer, dimension(2) :: tagTypes!  { DENSE_DOUBLE, DENSE_DOUBLE }
+         integer nverts(3), nelem(3), nblocks(3), nsbc(3), ndbc(3)
 
          ! readopts( "PARALLEL=READ_PART;PARTITION=PARALLEL_PARTITION;PARALLEL_RESOLVE_SHARED_ENTS" )
          ! readoptsLnd( "PARALLEL=READ_PART;PARTITION=PARALLEL_PARTITION" )
-         inputFileName = &
-            MOAB_MESH_DIR &
-            //'unittest/atm_c2x.h5m'//C_NULL_CHAR
-         readopts = 'PARALLEL=READ_PART;PARTITION=PARALLEL_PARTITION;PARALLEL_RESOLVE_SHARED_ENTS'//C_NULL_CHAR
-         num_layers = 2 ! two layer only
+         !inputFileName = &
+         !   MOAB_MESH_DIR &
+         !   //'unittest/atm_c2x.h5m'//C_NULL_CHAR
+         !readopts = 'PARALLEL=READ_PART;PARTITION=PARALLEL_PARTITION;' &
+         !           //'PARALLEL_RESOLVE_SHARED_ENTS'//C_NULL_CHAR
+         inputFileName = MOAB_MESH_DIR &
+              // 'unittest/io/domain.ocn.ne4np4_oQU240.160614.nc'//C_NULL_CHAR
+         readopts = 'PARALLEL=READ_PART;PARTITION_METHOD=SQIJ;VARIABLE=;' &
+                    //'REPARTITION'//C_NULL_CHAR
+
+         num_layers = 0 ! initialize to no ghost layer for now
 
          call MPI_Init(ierr)
          call errorout(ierr, 'fail to initialize MPI')
@@ -52,55 +58,68 @@
          ierr = iMOAB_Initialize()
          call errorout(ierr, 'fail to initialize iMOAB')
 
-         call MPI_COMM_RANK(MPI_COMM_WORLD, my_id, ierr)
+         call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierr)
          call errorout(ierr, 'fail to get MPI rank')
 
-         call MPI_COMM_SIZE(MPI_COMM_WORLD, num_procs, ierr)
+         call MPI_COMM_SIZE(MPI_COMM_WORLD, nprocs, ierr)
          call errorout(ierr, 'fail to get MPI size')
 
          ! find out MY process ID, and how many processes were started.
-         if (my_id .eq. 0) then
-            print *, " I'm process ", my_id, " out of ", &
-               num_procs, " processes."
+         if (rank .eq. 0) then
+            print *, " I'm process ", rank, " out of ", &
+               nprocs, " processes."
          end if
 
          !  give a component id
-         compid = 7
-         appname = 'IMTEST'//CHAR(0)
+         compid = 100
+         appname = 'HaloExchangeApp'//CHAR(0)
+
+         ! first, let us register the application
          ierr = iMOAB_RegisterApplication(appname, MPI_COMM_WORLD, &
                                           compid, pid)
          call errorout(ierr, 'fail to initialize fortran app')
 
+         ! load the mesh in parallel with specified options
          ierr = iMOAB_LoadMesh(pid, inputFileName, readopts, num_layers)
          call errorout(ierr, 'fail to load atm')
 
          !  see ghost elements
          dimgh = 2 ! will ghost quads, topological dim 2
+         num_layers = 2 ! let us get two ghost layers
          bridge_dim = 0 ! use vertex as bridge
-         if (my_id .eq. 0) then
+         if (rank .eq. 0) then
             print *, " Generating ", num_layers, " ghost layers"
          end if
          ierr = iMOAB_DetermineGhostEntities(pid, dimgh, num_layers, &
                                              bridge_dim)
          call errorout(ierr, 'fail to determine ghosts')
 
+         ! let us get some information about the partitioned mesh and print
+         ierr = iMOAB_GetMeshInfo(pid, nverts, nelem, nblocks, nsbc, ndbc)
+         call errorout(ierr, 'Error: failed to get mesh info ')
+         if (rank .eq. 0) then
+            print *,  "MOAB vertices: owned=", nverts(1), &
+                              ", ghosted=", nverts(2), ", total=", nverts(3)
+            print *,  "MOAB elements: owned=", nelem(1), &
+                              ", ghosted=", nelem(2), ", total=", nelem(3)
+         endif
+
          tagTypes(:) = 1 ! DENSE_DOUBLE
-         ierr = iMOAB_DefineTagStorage(pid, 'Sa_dens:Sa_pbot'//CHAR(0), tagTypes(1), 1, tagIndex(1))
+         ierr = iMOAB_DefineTagStorage(pid, 'Sa_dens:Sa_pbot'//CHAR(0), &
+                                       tagTypes(1), 1, tagIndex(1))
          call errorout(ierr, 'failed to define the field tag Sa_dens ')
 
          entity_type(:) = 1 ! data is on elements (vertices = 0, elements = 1)
-         ! let us now access the density field: Sa_dens
-         ! and exchange tags to ensure the data is consistent
+         ! let us now synchronize the density/pressure fields: Sa_dens, Sa_pbot
+         ! and exchange tag values to ensure the data is consistent in the ghost
+         ! halo regions
          ierr = iMOAB_SynchronizeTags(pid, 2, tagIndex, entity_type)
 
          ! write out the mesh file to disk, in parallel, if h5m
 #ifdef MOAB_HAVE_HDF5
-         write (outfile, '(A17,i1,A4)') 'ghost_exchanged_p', my_id, '.h5m'//CHAR(0)
-         !outfile = 'ghost_exchanged.h5m'//CHAR(0)
-         !wopts   = 'PARALLEL=WRITE_PART'//CHAR(0)
-         wopts = CHAR(0)
-         ierr = iMOAB_WriteMesh(pid, trim(outfile), wopts)
-         call errorout(ierr, 'fail to write the mesh file')
+         outfile = 'ghost_exchanged'//CHAR(0)
+         ierr = iMOAB_WriteLocalMesh(pid, trim(outfile))
+         call errorout(ierr, 'fail to write the local mesh files')
 #endif
 
          !  all done. de-register and finalize
