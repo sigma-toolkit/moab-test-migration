178393d9f47c3160e19b933a01a2dfed1c9b1a38
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2024-10-12 23:53:31 +0600

    move core init to root

--- pymoab/__init__.py
+++ pymoab/__init__.py
@@ -11,16 +11,46 @@ PyMOAB provides a means of interactively interrogating, modifying, and generatin
 MOAB mesh files.
 
 Much of the core functionality of MOAB has been implemented in the core.Core
-module. Interaction with this is intended to be largely analagous to interaction
+module. Interaction with this is intended to be largely analogous to interaction
 with MOAB via its native C++ API though some modifications have been made to
 allow for interaction with the interface using the various native Python
 constructs such as lists, tuples, etc.
-
 """
 
+import os
+import glob
+import sysconfig
 from importlib.metadata import version, PackageNotFoundError
 
 try:
     __version__ = version('MOAB')
 except PackageNotFoundError:
     __version__ = 'Please install this project with pip'
+
+def get_path(subdir, pattern="*"):
+    """Helper function to return paths that match a given pattern within a subdirectory."""
+    path = os.path.join(__path__[0], "core", subdir)
+    return glob.glob(os.path.join(path, pattern)) if os.path.exists(path) else []
+
+def get_include_path():
+    """Return the include directory path for MOAB headers."""
+    return os.path.join(__path__[0], "core", "include")
+
+def get_core_libraries():
+    """Return library paths and library directory paths."""
+    lib_paths = [os.path.join(__path__[0], "core", subdir) for subdir in ["lib", "lib64"]]
+    libs = [lib for subdir in lib_paths for lib in get_path(subdir)]
+    return libs, [path for path in lib_paths if os.path.exists(path)][0]
+
+def get_extra_libraries():
+    """List all the extra libraries of MOAB."""
+    libs_path = os.path.join(sysconfig.get_paths()['platlib'], "moab.libs")
+    return (glob.glob(os.path.join(libs_path, "*")), libs_path) if os.path.exists(libs_path) else ([], [])
+
+# Setup variables
+include_path = get_include_path()
+lib, lib_path = get_core_libraries()
+extra_lib, extra_lib_path = get_extra_libraries()
+
+# Export variables for easy access
+__all__ = ["include_path", "lib", "lib_path", "extra_lib", "extra_lib_path"]
