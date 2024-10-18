1f0ff9c675ab9d5d1941bee7a8d3ef44fb970028
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2024-10-18 19:46:53 +0600

    remove extra find packages

--- pymoab/__init__.py
+++ pymoab/__init__.py
@@ -27,30 +27,32 @@ try:
 except PackageNotFoundError:
     __version__ = "unknown"
 
-def get_path(subdir, pattern="*"):
+def get_core_path(subdir, pattern="*"):
     """Helper function to return paths that match a given pattern within a subdirectory."""
     path = os.path.join(__path__[0], "core", subdir)
     return glob.glob(os.path.join(path, pattern)) if os.path.exists(path) else []
 
 def get_include_path():
     """Return the include directory path for MOAB headers."""
-    return os.path.join(__path__[0], "core", "include")
+    include = get_core_path("include")
+    include_path = get_core_path("include", "")
+    return include, include_path
 
 def get_core_libraries():
     """Return library paths and library directory paths."""
-    lib_paths = [os.path.join(__path__[0], "core", subdir) for subdir in ["lib", "lib64"]]
-    libs = [lib for subdir in lib_paths for lib in get_path(subdir)]
-    return libs, [path for path in lib_paths if os.path.exists(path)][0]
+    lib = [libs for lib in ["lib", "lib64"] for libs in get_core_path(lib)]
+    lib_paths = [libs for lib in ["lib", "lib64"] for libs in get_core_path(lib, "")]
+    return lib, lib_paths
 
 def get_extra_libraries():
-    """List all the extra libraries of MOAB."""
+    """List all the extra libraries."""
     libs_path = os.path.join(__path__[0], ".dylibs") if sys.platform == "darwin" else os.path.join(__path__[0], "..", "moab.libs")
     return (glob.glob(os.path.join(libs_path, "*")), libs_path) if os.path.exists(libs_path) else ([], [])
 
 # Setup variables
-include_path = get_include_path()
+include, include_path = get_include_path()
 lib, lib_path = get_core_libraries()
 extra_lib, extra_lib_path = get_extra_libraries()
 
 # Export variables for easy access
-__all__ = ["include_path", "lib", "lib_path", "extra_lib", "extra_lib_path"]
+__all__ = ["include", "include_path", "lib", "lib_path", "extra_lib", "extra_lib_path"]
