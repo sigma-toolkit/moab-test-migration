a2f796e77571cbca64ad2b70a5a4ffb63bde70de
Iulian Grindeanu <iulian@mcs.anl.gov>
2025-02-03 18:20:25 -0600

    local changes to read area a and b from map file
    and set it later with a round of crystal gs to
    set aream tag

--- src/Remapping/TempestOnlineMapIO.cpp
+++ src/Remapping/TempestOnlineMapIO.cpp
@@ -1204,12 +1204,17 @@ void print_progress( const int barWidth, const float progress, const char* messa
 
 moab::ErrorCode moab::TempestOnlineMap::ReadParallelMap( const char* strSource,
                                                          const std::vector< int >& owned_dof_ids,
-                                                         bool row_partition )
+                                                         bool row_partition,
+                                                         std::vector<double> & vecAreaA,
+                                                         int & nA,
+                                                         std::vector<double> & vecAreaB,
+                                                         int & nB)
 {
     NcError error( NcError::silent_nonfatal );
 
     NcVar *varRow = NULL, *varCol = NULL, *varS = NULL;
-    int nS = 0, nA = 0, nB = 0;
+    NcVar *varAreaA = NULL, *varAreaB = NULL;
+    int nS = 0;
 #ifdef MOAB_HAVE_PNETCDF
     // some variables will be used just in the case netcdfpar reader fails
     int ncfile = -1;
@@ -1258,10 +1263,18 @@ moab::ErrorCode moab::TempestOnlineMap::ReadParallelMap( const char* strSource,
         varS = ncMap.get_var( "S" );
         CHECK_EXCEPTION( varS, "variable", "S" );
 
+        varAreaA = ncMap.get_var("area_a");
+        CHECK_EXCEPTION( varAreaA, "variable", "area_a" );
+
+        varAreaB = ncMap.get_var("area_b");
+        CHECK_EXCEPTION( varAreaB, "variable", "area_b" );
+
 #ifdef MOAB_HAVE_NETCDFPAR
         ncMap.enable_var_par_access( varRow, is_independent );
         ncMap.enable_var_par_access( varCol, is_independent );
         ncMap.enable_var_par_access( varS, is_independent );
+        ncMap.enable_var_par_access( varAreaA, is_independent );
+        ncMap.enable_var_par_access( varAreaB, is_independent );
 #endif
     }
     else
@@ -1279,6 +1292,9 @@ moab::ErrorCode moab::TempestOnlineMap::ReadParallelMap( const char* strSource,
         MPI_Offset leng;
         ERR_PARNC( ncmpi_inq_dimlen( ncfile, ins, &leng ) );
         nS  = (int)leng;
+        ERR_PARNC( ncmpi_inq_dimid( ncfile, "n_a", &ins ) );
+        ERR_PARNC( ncmpi_inq_dimlen( ncfile, ins, &leng ) );
+        nA = (int)leng;
         ERR_PARNC( ncmpi_inq_dimid( ncfile, "n_b", &ins ) );
         ERR_PARNC( ncmpi_inq_dimlen( ncfile, ins, &leng ) );
         nB = (int)leng;
@@ -1298,11 +1314,30 @@ moab::ErrorCode moab::TempestOnlineMap::ReadParallelMap( const char* strSource,
         localSize += nS % size;
     }
 
+    int localSizeA   = nA / size;
+    long offsetReadA = rank * localSizeA;
+    // leftovers on last rank
+    if( rank == size - 1 )
+    {
+        localSizeA += nA % size;
+    }
+
+    int localSizeB   = nB / size;
+    long offsetReadB = rank * localSizeB;
+    // leftovers on last rank
+    if( rank == size - 1 )
+    {
+        localSizeB += nB % size;
+    }
+
+
     std::vector< int > vecRow, vecCol;
     std::vector< double > vecS;
     vecRow.resize( localSize );
     vecCol.resize( localSize );
     vecS.resize( localSize );
+    vecAreaA.resize(localSizeA);
+    vecAreaB.resize(localSizeB);
 
     if( ncMap.is_valid() )
     {
@@ -1314,6 +1349,12 @@ moab::ErrorCode moab::TempestOnlineMap::ReadParallelMap( const char* strSource,
 
         varS->set_cur( (long)( offsetRead ) );
         varS->get( &( vecS[0] ), localSize );
+
+        varAreaA->set_cur( (long)( offsetReadA ) );
+        varAreaA->get( &( vecAreaA[0] ), localSizeA );
+
+        varAreaB->set_cur( (long)( offsetReadB ) );
+        varAreaB->get( &( vecAreaB[0] ), localSizeB );
 
         ncMap.close();
     }
@@ -1331,6 +1372,17 @@ moab::ErrorCode moab::TempestOnlineMap::ReadParallelMap( const char* strSource,
         ERR_PARNC( ncmpi_get_vara_int_all( ncfile, varid, &start, &count, &vecRow[0] ) );
         ERR_PARNC( ncmpi_inq_varid( ncfile, "col", &varid ) );
         ERR_PARNC( ncmpi_get_vara_int_all( ncfile, varid, &start, &count, &vecCol[0] ) );
+
+        ERR_PARNC( ncmpi_inq_varid( ncfile, "area_a", &varid ) );
+        MPI_Offset startA = (MPI_Offset)offsetReadA;
+        MPI_Offset countA = (MPI_Offset)localSizeA;
+        ERR_PARNC( ncmpi_get_vara_double_all( ncfile, varid, &startA, &countA, &vecAreaA[0] ) );
+
+        ERR_PARNC( ncmpi_inq_varid( ncfile, "area_b", &varid ) );
+        MPI_Offset startB = (MPI_Offset)offsetReadB;
+        MPI_Offset countB = (MPI_Offset)localSizeB;
+        ERR_PARNC( ncmpi_get_vara_double_all( ncfile, varid, &startB, &countB, &vecAreaB[0] ) );
+
         ERR_PARNC( ncmpi_close( ncfile ) );
 #endif
     }
--- src/moab/Remapping/TempestOnlineMap.hpp
+++ src/moab/Remapping/TempestOnlineMap.hpp
@@ -103,8 +103,12 @@ class TempestOnlineMap : public OfflineMap
     ///		Read the OfflineMap from a NetCDF file.
     ///	</summary>
     moab::ErrorCode ReadParallelMap( const char* strSource,
-                                     const std::vector< int >& owned_dof_ids,
-                                     bool row_major_ownership = true );
+                                     const std::vector< int >& tgt_dof_ids,
+                                     bool row_major_ownership,
+                                     std::vector<double> & areaA,
+                                     int & nA,
+                                     std::vector<double> & areaB,
+                                     int & nB);
 
     ///	<summary>
     ///		Write the TempestOnlineMap to a parallel NetCDF file.
