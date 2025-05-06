92f907822e4dff073a350f63b80bd4e54f7f71f9
Baptiste Mouginot <bam.git.no.reply@gmail.com>
2025-05-06 18:23:02 +0200

    formating

--- src/io/ReadRTT.cpp
+++ src/io/ReadRTT.cpp
@@ -313,7 +313,7 @@ ErrorCode ReadRTT::build_moab( std::vector< node > node_data,
     Tag mat_num_tag;
     Tag mat_name_tag;
     std::string mat_flag_name = get_material_ref_flag();
-    int mat_name_max_size     = get_max_name_size( cell_flag_datas[mat_flag_name] ) +1;
+    int mat_name_max_size     = get_max_name_size( cell_flag_datas[mat_flag_name] ) + 1;
     //  int zero = 0;
     rval = MBI->tag_get_handle( "MATERIAL_NUMBER", 1, MB_TYPE_INTEGER, mat_num_tag, MB_TAG_SPARSE | MB_TAG_CREAT );
     rval = MBI->tag_get_handle( "MATERIAL_NAME", mat_name_max_size, MB_TYPE_OPAQUE, mat_name_tag,
@@ -323,7 +323,7 @@ ErrorCode ReadRTT::build_moab( std::vector< node > node_data,
     Tag part_name_tag;
     Tag part_num_tag;
     std::string part_flag_name = get_container_ref_flag();
-    int max_part_name_size     = get_max_name_size( cell_flag_datas[part_flag_name] ) +1;
+    int max_part_name_size     = get_max_name_size( cell_flag_datas[part_flag_name] ) + 1;
     rval = MBI->tag_get_handle( "PART_NUMBER", 1, MB_TYPE_INTEGER, part_num_tag, MB_TAG_SPARSE | MB_TAG_CREAT );
     rval = MBI->tag_get_handle( "PART_NAME", max_part_name_size, MB_TYPE_OPAQUE, part_name_tag,
                                 MB_TAG_SPARSE | MB_TAG_CREAT );
@@ -588,7 +588,7 @@ ErrorCode ReadRTT::read_cell_flags( const char* filename, rtt_flags& cell_flags 
 
     // process the MATERIAL
     rval = ReadRTT::cell_process_flag( cell_flags, "MATERIAL" );
-    
+
     std::string material_flag_name = get_material_ref_flag();
     cell_data                      = cell_flag_datas[material_flag_name];
     cell_data_idx                  = cell_flag_indexes[material_flag_name];
