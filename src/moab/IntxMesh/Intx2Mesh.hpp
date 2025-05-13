60f55c448db8651036e89bbff71577bd5ec98cda
Iulian Grindeanu <iulian@mcs.anl.gov>
2025-05-13 17:42:45 -0500

    start logic for parallel edge splits logic
    covering set has a new argument, include edges in the
    coverage set
    still need to write the target files in parallel
    
    decide based on owned edges if to process or not the edge

--- src/moab/IntxMesh/Intx2Mesh.hpp
+++ src/moab/IntxMesh/Intx2Mesh.hpp
@@ -258,6 +258,7 @@ class Intx2Mesh
     TupleList* remote_cells;                       // not used anymore for communication, just a container
     TupleList* remote_cells_with_tracers;          // these will be used now to update tracers on remote procs
     std::map< int, EntityHandle > globalID_to_eh;  // needed for parallel, mostly
+    std::map< int, EntityHandle > globalID_to_edgeh;  // needed for parallel, mostly
 #endif
     int max_edges_1;  // maximum number of edges in the lagrange set (first set, src)
     int max_edges_2;  // maximum number of edges in the euler set (second set, tgt)
