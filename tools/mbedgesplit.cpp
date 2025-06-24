7634b76d5e485a7036e641484a5d12cf9b49b0a7
Iulian Grindeanu <iulian@mcs.anl.gov>
2025-06-24 14:47:57 -0500

    still not working in parallel
    primary variiable is owned cells, but edge data is
    repeated for shared edges anyway
    So we do not need to filter owned or not cells
    all local cells will write some data
    
    shared parallel edges will have their data written once by each task
    all other internal edges have the same data repeated anyway (locally)

--- tools/mbedgesplit.cpp
+++ tools/mbedgesplit.cpp
@@ -258,7 +258,18 @@ int main( int argc, char* argv[] )
         edgeVertices, edgePolygons, recoveredPolys, areaTolerance );MB_CHK_SET_ERR( rval, "failed to compute edge map for source" );
 #ifdef MOAB_HAVE_PNETCDF
 #ifdef MOAB_HAVE_MPI
-    rval = moab::IntxUtils::write_edge_map_parallel("source_edge.nc", pcomm, mb, sf1, edgeVertices, edgePolygons, recoveredPolys);MB_CHK_SET_ERR( rval, "failed to write edge map for source file" );
+    if (size > 1)
+    {
+       std::ostringstream file_str;
+       file_str << "source_edge_p" << pcomm->size() << ".nc";
+       rval = moab::IntxUtils::write_edge_map_parallel(file_str.str().c_str(), pcomm, mb, sf1, edgeVertices, edgePolygons, recoveredPolys);MB_CHK_SET_ERR( rval, "failed to write edge map for source file" );
+    }
+    else
+    {
+#ifdef MOAB_HAVE_NETCDF
+       rval = moab::IntxUtils::write_edge_map("source_edge.nc", mb, sf1, edgeVertices, edgePolygons, recoveredPolys);MB_CHK_SET_ERR( rval, "failed to write edge map for source file" );
+#endif
+    }
 #endif
 #else
 #ifdef MOAB_HAVE_NETCDF
@@ -278,7 +289,18 @@ int main( int argc, char* argv[] )
         edgeVertices, edgePolygons, recoveredPolys, areaTolerance );MB_CHK_SET_ERR( rval, "failed to compute edge map for target" );
 #ifdef MOAB_HAVE_PNETCDF
 #ifdef MOAB_HAVE_MPI
-    rval = moab::IntxUtils::write_edge_map_parallel("target_edge.nc", pcomm, mb, sf2, edgeVertices, edgePolygons, recoveredPolys);MB_CHK_SET_ERR( rval, "failed to write edge map for target" );
+    if (size > 1)
+    {
+        std::ostringstream file_str;
+        file_str << "target_edge_p" << pcomm->size() << ".nc";
+        rval = moab::IntxUtils::write_edge_map_parallel(file_str.str().c_str(), pcomm, mb, sf2, edgeVertices, edgePolygons, recoveredPolys);MB_CHK_SET_ERR( rval, "failed to write edge map for target" );
+    }
+    else
+    {
+#ifdef MOAB_HAVE_NETCDF
+        rval = moab::IntxUtils::write_edge_map("target_edge.nc", mb, sf2, edgeVertices, edgePolygons, recoveredPolys);MB_CHK_SET_ERR( rval, "failed to write edge map for target" );
+#endif
+    }
 #endif
 #else
 #ifdef MOAB_HAVE_NETCDF
