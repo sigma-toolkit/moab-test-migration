2a098547af9b0176743a9f9d55ba734e1a4b3477
Vijay Mahadevan <vijay.m@gmail.com>
2025-11-12 14:43:13 -0500

    Fix dist and distcheck targets to work correctly

--- moab.make.cmake
+++ moab.make.cmake
@@ -24,7 +24,7 @@ MOAB_INCLUDES = -I@abs_srcdir@/src \
                 -I@abs_srcdir@/src/LocalDiscretization \
                 -I@abs_srcdir@/src/RefineMesh
 
-MOAB_INCLUDES += @ZOLTAN_INCLUDES@
+MOAB_INCLUDES += -I@ZOLTAN_INCLUDES@ -I@NETCDF_INCLUDES@ -I@PNETCDF_INCLUDES@ -I@HDF5_INCLUDES@
 
 MOAB_CPPFLAGS = @CPPFLAGS@ 
 MOAB_CXXFLAGS = @CXXFLAGS@ 
