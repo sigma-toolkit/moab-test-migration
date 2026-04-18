801198742f8b68b276de3d8857ae626a1a040009
Vijay Mahadevan <vijay.m@gmail.com>
2026-04-18 03:21:17 -0400

    build: set MOAB_HAVE_NETCDFPAR in CMake when NetCDF/HDF5 parallel is on
    Detect via `nc-config --has-parallel4` with netcdf_par.h header
    fallback. Matches autotools' AC_CHECK_HEADERS so parallel SCRIP map
    writes use ParNcFile instead of crashing on serial NcFile.

--- config/FindNETCDF.cmake
+++ config/FindNETCDF.cmake
@@ -89,4 +89,24 @@ find_package_handle_standard_args(NETCDF
     VERSION_VAR NETCDF_VERSION
 )
 
+# Check for parallel NetCDF support via nc-config or header presence
+if (NETCDF_INCLUDES)
+    # Prefer nc-config --has-parallel4 (checks if NetCDF was built with parallel HDF5)
+    if (NC_CONFIG_EXECUTABLE)
+        execute_process(
+            COMMAND ${NC_CONFIG_EXECUTABLE} --has-parallel4
+            OUTPUT_VARIABLE _nc_has_parallel4
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            ERROR_QUIET)
+        if ("${_nc_has_parallel4}" STREQUAL "yes")
+            set(NETCDF_PARALLEL TRUE)
+        endif()
+    else()
+        # Fallback: check for netcdf_par.h header
+        if (EXISTS "${NETCDF_INCLUDES}/netcdf_par.h")
+            set(NETCDF_PARALLEL TRUE)
+        endif()
+    endif()
+endif()
+
 mark_as_advanced(NETCDF_INCLUDES NETCDF_LIBRARIES)
